31475a31d4e34e17cc608321ac3900c6a9d305b0
MathieuAA <mathieu.aa@free.fr>
2014-12-28 13:36:42 +0100

    Removing an item from the shopping cart is now possible. (+refactorings).

--- TWClient/WebContent/script/script.js
+++ TWClient/WebContent/script/script.js
@@ -2,8 +2,8 @@ $(document).ready(
 	function() { 
 		$("table").tablesorter();
 		if (sessionStorage.books) {
-			var books = JSON.parse(sessionStorage.getItem("books"));
-			$("#book-ids-field").attr("value", JSON.stringify(books));
+			var books = getBooksFromSessionStorage();
+			updateHiddenFieldValue(books);
 			$("#cart-size").text("" + books.length);
 			if (books.length == 0) {
 				$("#cart-display-button").attr("disabled", "disabled");
@@ -17,21 +17,45 @@ $(document).ready(
 ); 
 
 function addToCart(id, title) {
+	var books = new Array();
 	if (!sessionStorage.books) {
-		var books = new Array();
 		books[0] = id;
-		sessionStorage.setItem("books", JSON.stringify(books));
 		$("#book-ids-field").attr("value", JSON.stringify(books));
 		$("#cart-size").text("1");
 		$("#cart-display-button").removeAttr("disabled");
 	} else {
-		var books = JSON.parse(sessionStorage.getItem("books"));
+		books = getBooksFromSessionStorage();
 		books[books.length] = id;
-		sessionStorage.setItem("books", JSON.stringify(books));
-		$("#book-ids-field").attr("value", JSON.stringify(books));
+		updateHiddenFieldValue(books);
 		$("#cart-size").text(parseInt($("#cart-size").text()) + 1);
 	}
+	updateSessionStorage(books);
 	alert("The book " + title + " is added to your cart.");
+}
+
+function removeFromCart(id, title) {
+	if (sessionStorage.books) {
+		var books = getBooksFromSessionStorage();
+		var index = books.indexOf(id);
+		if (index > -1) {
+			books.splice(index, 1);
+			updateHiddenFieldValue(books);
+			updateSessionStorage(books);
+			alert("The book " + title + " is now removed from your cart.");
+		}
+	}
+}
+
+function getBooksFromSessionStorage() {
+	return JSON.parse(sessionStorage.getItem("books"));
+}
+
+function updateSessionStorage(books) {
+	sessionStorage.setItem("books", JSON.stringify(books));
+}
+
+function updateHiddenFieldValue(books) {
+	$("#book-ids-field").attr("value", JSON.stringify(books));
 }
 
 function displayHiddenSearchFields() {
