6151fe047f2dedcaae1a487aa5ea91fa07720ff2
MathieuAA <mathieu.aa@free.fr>
2014-12-29 16:21:40 +0100

    getLightBook by id is written.

--- TWInterfaces/src/main/java/fr/upem/m2/tw/mlvbooks/services/BookService.java
+++ TWInterfaces/src/main/java/fr/upem/m2/tw/mlvbooks/services/BookService.java
@@ -7,6 +7,7 @@ import fr.upem.m2.tw.mlvbooks.objects.dto.LightBookDTO;
 import fr.upem.m2.tw.mlvbooks.objects.dto.SearchParametersDTO;
 
 public interface BookService {
+  public LightBookDTO getLightBook(Long id);
   public List<LightBookDTO> getBooks(SearchParametersDTO searchParametersDTO);
-  public HeavyBookDTO getBook(String ISBN);
+  public HeavyBookDTO getBook(Long id);
 }
