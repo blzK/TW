aa6bfec292c2ac3c051ddccc02c3cbe207a238e3
MathieuAA <mathieu.aa@free.fr>
2015-01-03 16:12:18 +0100

    TWDistrib src and pom finished.

--- TWDistributedLibraryServer/src/fr/upem/m2/tw/library/server/main/Library.java
+++ TWDistributedLibraryServer/src/fr/upem/m2/tw/library/server/main/Library.java
@@ -1,49 +1,48 @@
 package fr.upem.m2.tw.library.server.main;
 
-
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 
-import fr.upem.m2.tw.mlvbooks.objects.dto.BookDTO;
-
+import fr.upem.m2.tw.mlvbooks.objects.dto.HeavyBookDTO;
+import fr.upem.m2.tw.mlvbooks.objects.dto.LightBookDTO;
+import fr.upem.m2.tw.mlvbooks.objects.dto.SearchParametersDTO;
 
 /**
- * Provides methods to drive the application book library (consult, add, remove).
- * Please add new features via this interface.
+ * Provides methods to drive the application book library (consult, add,
+ * remove). Please add new features via this interface.
+ * 
  * @author Alexandre SALIMI (alexander.salimi@gmail.com)
  */
 
-public interface Library extends Remote{
-	
-	/**
-	 * Add a heavy book to the library. This book representation offers more
-	 * properties than a light one. Useful for internal app data management.
-	 * @param id which must be manually provided (see the client example).
-	 * @param title of the book
-	 * @throws RemoteException
-	 */
-	public boolean addHeavyBook(Long id, String title, String author) throws RemoteException;
-	
-	/**
-	 * Add a heavy book to the library. This book representation offers more
-	 * properties than a light one. Useful for internal app data management.
-	 * @param id which must be manually provided (see the client example).
-	 * @param title of the book
-	 * @throws RemoteException
-	 */
-	public boolean addLightBook(Long id, String title, String author) throws RemoteException;
-	
-	public boolean addExistingBook(BookDTO book) throws RemoteException;
-	
-	public void remove(Long isbn) throws RemoteException;
-	
-	public List<BookDTO> searchById(Long Id) throws RemoteException;
-	
-	public List<BookDTO> searchByTitle(String title) throws RemoteException;
-	
-	public List<BookDTO> searchByAuthor(String title) throws RemoteException;
-	
-	public List<BookDTO> searchByPriceRange(Long minimum, Long maximum);
-	
+public interface Library extends Remote {
+
+  /**
+   * Add a heavy book to the library. This book representation offers more
+   * properties than a light one. Useful for internal app data management.
+   * 
+   * @param id which must be manually provided (see the client example).
+   * @param title of the book
+   * @throws RemoteException
+   */
+  public boolean addBook(Long id, String title, String author)
+      throws RemoteException;
+
+  public boolean addExistingBook(HeavyBookDTO book) throws RemoteException;
+
+  public void remove(Long isbn) throws RemoteException;
+
+  public LightBookDTO searchLightBookById(Long id) throws RemoteException;
+
+  public HeavyBookDTO searchHeavyBookById(Long id) throws RemoteException;
+
+  public List<LightBookDTO> searchBook(SearchParametersDTO searchParametersDTO)
+      throws RemoteException;
+  
+  public List<LightBookDTO> searchByTitle(String title) throws RemoteException;
+
+  public List<LightBookDTO> searchByAuthor(String title) throws RemoteException;
+
+  public LightBookDTO searchByISBN(String ISBN) throws RemoteException;
+
 }
--- TWDistributedLibraryServer/src/fr/upem/m2/tw/library/server/main/LibraryImp.java
+++ TWDistributedLibraryServer/src/fr/upem/m2/tw/library/server/main/LibraryImp.java
@@ -1,130 +1,148 @@
 package fr.upem.m2.tw.library.server.main;
 
-
 import java.io.Serializable;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
-import fr.upem.m2.tw.mlvbooks.objects.dto.BookDTO;
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+
 import fr.upem.m2.tw.mlvbooks.objects.dto.HeavyBookDTO;
 import fr.upem.m2.tw.mlvbooks.objects.dto.LightBookDTO;
+import fr.upem.m2.tw.mlvbooks.objects.dto.SearchParametersDTO;
 
-public class LibraryImp extends UnicastRemoteObject implements Library, Serializable {
+public class LibraryImp extends UnicastRemoteObject implements Library,
+    Serializable {
 
-	/**
-	 * Serial UID
-	 */
-	private static final long serialVersionUID = 899577144233055923L;
-	
-	private final Map<Long, BookDTO> books;
+  /**
+   * Serial UID
+   */
+  private static final long serialVersionUID = 899577144233055923L;
 
-	protected LibraryImp() throws RemoteException {
-		super();
-		books = new HashMap<Long, BookDTO>();
-	}
+  private final Map<Long, HeavyBookDTO> books;
 
-	@Override
-	public boolean addLightBook(Long id, String title, String author)
-			throws RemoteException {
-		Objects.requireNonNull(id);
-		BookDTO book = new LightBookDTO(id, title, author);
-		if (books.containsKey(book.getId()))
-			return false;
-		books.put(book.getId(), book);
-		return true;
-	}
+  private Mapper mapper = new DozerBeanMapper();
 
-	@Override
-	public boolean addHeavyBook(Long id, String title, String author)
-			throws RemoteException {
-		Objects.requireNonNull(id);
-		BookDTO book = new HeavyBookDTO(id, title, author);
-		if (books.containsKey(book.getId()))
-			return false;
-		books.put(book.getId(), book);
-		return true;
-	}
+  protected LibraryImp() throws RemoteException {
+    super();
+    books = new ConcurrentHashMap<>();
+  }
 
-	@Override
-	public boolean addExistingBook(BookDTO book)
-			throws RemoteException {
-		Objects.requireNonNull(book.getId());
-		if (books.containsKey(book.getId()))
-			return false;
-		books.put(book.getId(), book);
-		return true;
-	}
+  @Override
+  public boolean addBook(Long id, String title, String author)
+      throws RemoteException {
+    Objects.requireNonNull(id);
+    HeavyBookDTO book = new HeavyBookDTO();
+    book.setId(id);
+    book.setAuthor(author);
+    book.setTitle(title);
+    if (books.containsKey(book.getId()))
+      return false;
+    books.put(book.getId(), book);
+    return true;
+  }
 
-	@Override
-	public void remove(Long isbn)
-			throws RemoteException {
-		if (books.containsKey(isbn))
-			books.remove(isbn);
-	}
+  @Override
+  public boolean addExistingBook(HeavyBookDTO book) throws RemoteException {
+    Objects.requireNonNull(book.getId());
+    if (books.containsKey(book.getId()))
+      return false;
+    books.put(book.getId(), book);
+    return true;
+  }
 
-	@Override
-	public List<BookDTO> searchById(Long id)
-			throws RemoteException {
-		Objects.requireNonNull(id);
-		if (books.isEmpty())
-			return null;
-		List<BookDTO> results = new ArrayList<BookDTO>();
-		for (Long elementId: books.keySet()) {
-			BookDTO book = books.get(id);
-			if (book.getId().equals(elementId))
-				results.add(book);
-		}
-		return results;
-	}
+  @Override
+  public void remove(Long id) throws RemoteException {
+    books.remove(id);
+  }
 
-	@Override
-	public List<BookDTO> searchByTitle(String title) throws RemoteException{
-		Objects.requireNonNull(title);
-		if (books.isEmpty())
-			return null;
-		List<BookDTO> results = new ArrayList<BookDTO>();
-		for (Long elementId: books.keySet()) {
-			BookDTO book = books.get(elementId);
-			if (book.getTitle().equals(title))
-				results.add(book);
-		}
-		return results;
-	}
+  @Override
+  public LightBookDTO searchLightBookById(Long id) throws RemoteException {
+    if (books.isEmpty() || id == null) {
+      return null;
+    }
+    HeavyBookDTO heavyBook = books.get(id);
+    if (heavyBook == null) {
+      return null;
+    }
+    return mapper.map(heavyBook, LightBookDTO.class);
+  }
 
-	@Override
-	public List<BookDTO> searchByAuthor(String author) throws RemoteException{
-		Objects.requireNonNull(author);
-		if (books.isEmpty())
-			return null;
-		List<BookDTO> results = new ArrayList<BookDTO>();
-		for (Long elementId: books.keySet()) {
-			BookDTO book = books.get(elementId);
-			if (book.getAuthor().equals(author))
-				results.add(book);
-		}
-		return results;
-	}
-	
-	@Override
-	public List<BookDTO> searchByPriceRange(Long minimum, Long maximum) {
-		Objects.requireNonNull(minimum);
-		Objects.requireNonNull(maximum);
-		if (books.isEmpty())
-			return null;
-		List<HeavyBookDTO> results = new ArrayList<HeavyBookDTO>();
-		for (Long elementId: books.keySet()) {
-			HeavyBookDTO heavyBook = books.get(elementId);
-			//Problème avec les HeavyBook 
-			if 
-			if (book.getAuthor().equals(author))
-				results.add(book);
-		}
-		return results;
-	}
+  @Override
+  public HeavyBookDTO searchHeavyBookById(Long id) throws RemoteException {
+    if (books.isEmpty() || id == null) {
+      return null;
+    }
+    return books.get(id);
+  }
 
+  @Override
+  public List<LightBookDTO> searchByTitle(String title) throws RemoteException {
+    if (books.isEmpty() || title == null)
+      return null;
+    List<LightBookDTO> results = new ArrayList<>();
+    for (Long elementId : books.keySet()) {
+      HeavyBookDTO book = books.get(elementId);
+      if (book.getTitle().equals(title)) {
+        results.add(mapper.map(book, LightBookDTO.class));
+      }
+    }
+    return results;
+  }
+
+  @Override
+  public List<LightBookDTO> searchByAuthor(String author)
+      throws RemoteException {
+    if (books.isEmpty() || author == null)
+      return null;
+    List<LightBookDTO> results = new ArrayList<>();
+    for (Long elementId : books.keySet()) {
+      HeavyBookDTO book = books.get(elementId);
+      if (book.getAuthor().equals(author))
+        results.add(mapper.map(book, LightBookDTO.class));
+    }
+    return results;
+  }
+
+  @Override
+  public LightBookDTO searchByISBN(String ISBN) throws RemoteException {
+    if (books.isEmpty() || ISBN == null)
+      return null;
+    for (Long elementId : books.keySet()) {
+      HeavyBookDTO book = books.get(elementId);
+      if (book.getISBN().equals(ISBN))
+        return mapper.map(book, LightBookDTO.class);
+    }
+    return null;
+  }
+
+  @Override
+  public List<LightBookDTO> searchBook(SearchParametersDTO searchParametersDTO)
+      throws RemoteException {
+    if (searchParametersDTO == null || books == null) {
+      return null;
+    }
+    List<LightBookDTO> searchedByTitle = null;
+    List<LightBookDTO> searchedByAuthor = null;
+    List<LightBookDTO> searchedByISBN = null;
+    if (searchParametersDTO.getTitle() != null) {
+      searchedByTitle = searchByTitle(searchParametersDTO.getTitle());
+    }
+    if (searchParametersDTO.getAuthor() != null) {
+      searchedByAuthor = searchByAuthor(searchParametersDTO.getAuthor());
+    }
+    if (searchParametersDTO.getISBN() != null) {
+      LightBookDTO book = searchByISBN(searchParametersDTO.getISBN());
+      searchedByISBN = (book == null) ? null : Arrays.asList(book);
+    }
+    searchedByTitle.retainAll(searchedByAuthor);
+    searchedByTitle.retainAll(searchedByISBN);
+    return (searchedByTitle.isEmpty()) ? null : searchedByTitle;
+  }
 }
